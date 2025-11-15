fdf18b838cd530758c4e19e9d998d0f96ccf8884
박부건 parkbugeon <bagdoeon827@gmail.com>
2025-11-15 23:00:21 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"], // 또는 src 경로라면 "./src/pages/..."로 변경
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
