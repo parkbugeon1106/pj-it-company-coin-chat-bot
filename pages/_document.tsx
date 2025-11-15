d7f1656849e4e911dfc8558a1f63e270689af18b
박부건 parkbugeon <bagdoeon827@gmail.com>
2025-11-15 23:06:04 +0900

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        {/* Google Fonts: Noto Sans KR */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
